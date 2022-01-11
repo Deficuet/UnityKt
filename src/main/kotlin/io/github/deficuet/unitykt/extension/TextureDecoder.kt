c98354bb9117a9841638df6858eccedef1c23bf5
Deficuet <36525579+Deficuet@users.noreply.github.com>
2022-01-11 13:45:21 -0700

    Delete TextureDecoder.kt

--- src/main/kotlin/io/github/deficuet/unitykt/extension/TextureDecoder.kt
+++
@@ -1,8 +0,0 @@
-package io.github.deficuet.unitykt.extension
-
-class TextureDecoder private constructor() {
-//    companion object {
-//        init { System.loadLibrary("TextureDecoder") }
-//        external fun decodeETC2A8(data: ByteArray, width: Int, height: Int, out: ByteArray)
-//    }
-}
