9565eeb6267fbae5d2d673977afb81db27fae91d
Deficuet <36525579+Deficuet@users.noreply.github.com>
2022-01-14 00:25:08 -0700

    Add files via upload

--- TextureDecoderNative/jni.cpp
+++ TextureDecoderNative/jni.cpp
@@ -1,1 +1,21 @@
+#include "io_github_deficuet_unitykt_extension_TextureDecoder.h"
+#include <stdint.h>
+#include <iostream>
+using namespace std;
 
+uint8_t* JavaByteArrayAsUInt8_T(JNIEnv *env, jbyteArray array) {
+    int len = env -> GetArrayLength(array);
+    uint8_t *p = new uint8_t[len];
+    env -> GetByteArrayRegion(array, 0, len, reinterpret_cast<jbyte *>(p));
+    return p;
+}
+
+JNIEXPORT void JNICALL Java_io_github_deficuet_unitykt_extension_TextureDecoder_decodeETC2A8(
+    JNIEnv *env, jclass clazz, jbyteArray data, jint w, jint h, jbyteArray out
+) {
+    uint8_t *pData = JavaByteArrayAsUInt8_T(env, data);
+    cout << (int) *(pData) << endl;
+    pData ++;
+    cout << (int) *(pData) << endl;
+    
+}
