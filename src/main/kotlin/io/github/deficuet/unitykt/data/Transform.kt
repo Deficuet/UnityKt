09686ada7accd9a5e6fab88b966944a35ced3f7e
Deficuet <36525579+Deficuet@users.noreply.github.com>
2022-01-19 13:50:29 -0700

    Add files via upload

---
+++ src/main/kotlin/io/github/deficuet/unitykt/data/Transform.kt
@@ -0,0 +1,19 @@
+package io.github.deficuet.unitykt.data
+
+import io.github.deficuet.unitykt.dataImpl.TransformImpl
+import io.github.deficuet.unitykt.file.ObjectInfo
+import io.github.deficuet.unitykt.file.SerializedFile
+import io.github.deficuet.unitykt.util.ObjectReader
+
+open class Transform internal constructor(
+    private val container: ImplementationContainer<TransformImpl>
+): Component(container) {
+    internal constructor(assetFile: SerializedFile, info: ObjectInfo):
+        this(ImplementationContainer { TransformImpl(ObjectReader(assetFile, info)) })
+
+    val mLocalRotation get() = container.impl.mLocalRotation
+    val mLocalPosition get() = container.impl.mLocalPosition
+    val mLocalScale get() = container.impl.mLocalScale
+    val mChildren get() = container.impl.mChildren
+    val mFather get() = container.impl.mFather
+}
