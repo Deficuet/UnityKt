9a29f4f6ce8273d9145606858bd49d43f0640f8d
Deficuet <36525579+Deficuet@users.noreply.github.com>
2022-01-21 13:48:56 -0700

    Add files via upload

--- src/main/kotlin/io/github/deficuet/unitykt/data/PPtr.kt
+++ src/main/kotlin/io/github/deficuet/unitykt/data/PPtr.kt
@@ -1,9 +1,12 @@
 package io.github.deficuet.unitykt.data
 
+import io.github.deficuet.unitykt.AssetManager
+import io.github.deficuet.unitykt.ImportContext
 import io.github.deficuet.unitykt.dataImpl.ObjectImpl
 import io.github.deficuet.unitykt.file.FormatVersion
 import io.github.deficuet.unitykt.file.SerializedFile
 import io.github.deficuet.unitykt.util.*
+import java.io.File
 
 class PPtr<T: Object> internal constructor(reader: ObjectReader) {
     var mFileID = reader.readInt()
@@ -14,6 +17,44 @@ class PPtr<T: Object> internal constructor(reader: ObjectReader) {
     val assetFile = reader.assetFile
 
     @PublishedApi internal var obj: T? = null
+
+    @PublishedApi internal fun getManager(): SerializedFile? {
+        return if (mFileID == 0) {
+            assetFile
+        } else if (mFileID > 0 && mFileID - 1 < assetFile.externals.size) {
+            val parent = assetFile.bundleParent
+            val name = assetFile.externals[mFileID - 1].name
+            if (parent !is ImportContext) {
+                (parent.files.tryGetOrUppercase(name) as? SerializedFile).let {
+                    if (it == null) {
+                        val path = parent.bundleParent.root.directory
+                        if (path.isNotEmpty()) {
+                            val actualName = StringRef()
+                            if (path.listFiles().containsIgnoreCase(name, actualName)) {
+                                val new = AssetManager.loadFile("$path/${actualName.value}")
+                                new.files.getValue(actualName.value) as SerializedFile
+                            } else null
+                        } else null
+                    } else it
+                }
+            } else {
+                AssetManager.assetFiles.tryGetOrUppercase(name).let {
+                    if (it == null) {
+                        val new = if (File("${parent.directory}/$name").exists()) {
+                            AssetManager.loadFile("${parent.directory}/$name")
+                        } else if (File("${parent.directory}/${name.uppercase()}").exists()) {
+                            AssetManager.loadFile("${parent.directory}/${name.uppercase()}")
+                        } else null
+                        new?.files?.tryGetOrUppercase(name)!!.let { externalFile ->
+                            if (externalFile is SerializedFile) {
+                                externalFile
+                            } else null
+                        }
+                    } else it
+                }
+            }
+        } else { null }
+    }
 }
 
 internal inline fun <reified O: Object> PPtr<O>.setObjInfo(impl: ObjectImpl) {
