a3061aa822c6f705f7da509819d3b7bf24d10a39
Deficuet <36525579+Deficuet@users.noreply.github.com>
2022-03-23 23:51:37 -0600

    add data for AssetBundle object

--- src/main/kotlin/io/github/deficuet/unitykt/data/AssetBundle.kt
+++ src/main/kotlin/io/github/deficuet/unitykt/data/AssetBundle.kt
@@ -1,7 +1,6 @@
 package io.github.deficuet.unitykt.data
 
-import io.github.deficuet.unitykt.dataImpl.AssetBundleImpl
-import io.github.deficuet.unitykt.dataImpl.AssetInfo
+import io.github.deficuet.unitykt.dataImpl.*
 import io.github.deficuet.unitykt.file.ObjectInfo
 import io.github.deficuet.unitykt.file.SerializedFile
 import io.github.deficuet.unitykt.util.ObjectReader
@@ -14,4 +13,11 @@ class AssetBundle private constructor(
 
     val mPreloadTable: Array<PPtr<Object>>          get() = container.impl.mPreloadTable
     val mContainer: Array<Pair<String, AssetInfo>>  get() = container.impl.mContainer
+    val mMainAsset: AssetInfo                       get() = container.impl.mMainAsset
+    val mRuntimeCompatibility: UInt                 get() = container.impl.mRuntimeCompatibility
+    val mAssetBundleName: String                    get() = container.impl.mAssetBundleName
+    val mDependencies: Array<String>                get() = container.impl.mDependencies
+    val mIsStreamedSceneAssetBundle: Boolean        get() = container.impl.mIsStreamedSceneAssetBundle
+    val mExplicitDataLayout: Int                    get() = container.impl.mExplicitDataLayout
+    val mPathFlags: Int                             get() = container.impl.mPathFlags
 }
